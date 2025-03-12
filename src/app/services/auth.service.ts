f97751b0f7e9a5ae70fb3b4f44497a12fc04dc7e
SyondaimeM <neoz.son@gmail.com>
2025-03-12 10:19:25 -0400

    create authenication service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8000/api';
+
+  constructor(private http: HttpClient) {}
+
+  login(data: any) {
+    return this.http.post(`${this.apiUrl}/login`, data);
+  }
+}
