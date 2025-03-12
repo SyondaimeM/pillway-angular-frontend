042986d61e9b1f0eaaed5474e1bb6f8f525cac3b
SyondaimeM <neoz.son@gmail.com>
2025-03-12 18:19:05 -0400

    updated the layout with nav bar

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  constructor(private router: Router) {}
 
+  logout() {
+    try {
+      // Remove the token from local storage
+      const tokenKey = 'angular18Token';
+      console.log('Before removal:', JSON.stringify(localStorage));
+      localStorage.removeItem(tokenKey);
+      console.log('After removal:', JSON.stringify(localStorage));
+      console.log(`${tokenKey} removed from local storage.`);
+      alert('done');
+    } catch (error) {
+      console.error('Error removing token from localStorage:', error);
+    }
+
+    // Redirect to the login page
+    this.router.navigate(['/login']).then(() => {
+      console.log('Navigation to login page completed');
+    }).catch(err => {
+      console.error('Navigation error:', err);
+    });
+  }
 }
